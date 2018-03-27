5377947609e02dd7292ad9f9d98e270e8481f843
EthanW0411 <wlm454529340@gmail.com>
2018-03-27 17:04:19 +1100

    add team member names

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,7 @@
 
 module.exports.homePage = function(req, res){
-    res.send("Happy Vibes Co.");
+    res.send("Happy Vibes Co.\n Lianming Wu, Alex Hurbe, Vishal Egbert");
+
 };
 module.exports.sayGoodbye = function(req, res) {
     res.send("Goodbye");
