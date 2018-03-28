576c134007bd44f558cf6ca7ed099c4a97565855
EthanW0411 <wlm454529340@gmail.com>
2018-03-29 01:04:15 +1100

    fix error on path

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,7 @@ const soonInformation = require("../models/comingSoonInfo.js");
 
 module.exports.comingSoon = function(req, res){
     //res.send("Happy Vibes Co.");
-    res.render("comingSoonPInforTemp", {info: soonInformation[0]});
+    res.render("comingSoonInforTemp", {info: soonInformation[0]});
 };
 
 module.exports.sayGoodbye = function(req, res) {
