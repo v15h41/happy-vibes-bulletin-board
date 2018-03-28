78d34f2a4f0918cf03fdcf89c759785e9eab5d76
EthanW0411 <wlm454529340@gmail.com>
2018-03-29 00:58:58 +1100

    change res.send info for testing

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,8 @@
 const soonInformation = require("../models/comingSoonInfo.js");
 
 module.exports.comingSoon = function(req, res){
-    //res.send(members);
-    res.render("ComingSoonPageView", {info: soonInformation[0]});
+    res.send("Happy Vibes Co.");
+    //res.render("ComingSoonPageView", {info: soonInformation[0]});
 };
 
 module.exports.sayGoodbye = function(req, res) {
