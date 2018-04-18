87c4edc9f6bb5e12508f33f043e8ca898f65d9dc
Vishal Egbert <vegbert@student.unimelb.edu.au>
2018-04-18 18:01:57 +1000

    create new note functionality

--- views/board_page.js
+++ views/board_page.js
@@ -1,19 +1,35 @@
+function open_new_note_overlay() {
+    console.log(document.getElementById("note_submit_overlay"));
+    document.getElementById("note_submit_overlay").style.display = "block";
+}
+
+function exit_note_submit() {
+    document.getElementById("sticky_submit_text").innerText = '';
+    document.getElementById("note_submit_overlay").style.display = "none";
+}
+
 function create_postit() {
+    var text = document.getElementById("sticky_submit_text").innerText;
+    exit_note_submit();
+
     var postitsdiv = document.getElementById('postits');
     console.log(document.getElementById('postits').offsetHeight);
     console.log(document.getElementById('postits').offsetWidth);
-    var div = document.createElement("DIV");
-    div.className = "sticky";
+    var sticky = document.createElement("DIV");
+    sticky.className = "sticky";
     var p = document.createElement("P");
-    p.appendChild(document.createTextNode("thank u person"));
-    div.appendChild(p);
+    p.appendChild(document.createTextNode(text));
+    sticky_text = document.createElement("DIV");
+    sticky_text.className = "sticky_text";
+    sticky_text.appendChild(p);
+    sticky.appendChild(sticky_text);
     var ran_height = Math.floor(Math.random()*(postitsdiv.offsetHeight-250)) + 1 ;
     var ran_width = Math.floor(Math.random()*(postitsdiv.offsetWidth-250)) + 1;
     console.log(ran_height, ran_height+'px');
     console.log(ran_width, ran_width+'px');
-    div.style.top = ran_height+'px';
-    div.style.left = ran_width+'px';
-    postitsdiv.appendChild(div);
-
-
+    sticky.style.top = ran_height+'px';
+    sticky.style.left = ran_width+'px';
+    postitsdiv.appendChild(sticky);
 }
+
+
