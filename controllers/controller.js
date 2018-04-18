467c8bbd1b537fcb77b94f2db346d783f8c28db6
EthanW0411 <wlm454529340@gmail.com>
2018-04-18 21:18:26 +1000

    test for heroku open

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,11 @@
 const soonInformation = require("../models/comingSoonInfo.js");
 
+/*
 module.exports.comingSoon = function(req, res){
     //res.send("Happy Vibes Co.");
     res.render("comingSoonInforTemp", {info: soonInformation[0]});
 };
+*/
 
 module.exports.login = function(req, res){
     //res.send("Happy Vibes Co.");
