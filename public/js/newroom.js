3ca5b8a58ef7b858ea5dff9a29f6db564e619e60
Vishal Egbert <vegbert@student.unimelb.edu.au>
2018-04-30 16:48:16 +1000

    added new files

---
+++ public/js/newroom.js
@@ -0,0 +1,9 @@
+function complete_new_user() {
+    document.getElementById("buttons_to_hide").style.display = "none";
+    document.getElementById("create_account_frame").style.display = "block";
+}
+
+function complete_existing_user() {
+    document.getElementById("buttons_to_hide").style.display = "none";
+    document.getElementById("existing_account_frame").style.display = "block";
+}
